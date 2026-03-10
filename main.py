f1cd850b78c751671b7f5eef412c666d88ba9bfd
mohib008 <mohib.0008@gmail.com>
2026-03-10 12:37:28 -0400

    new fill updated

--- main.py
+++ main.py
@@ -3,6 +3,15 @@
 #    for items in arr:
 #        print(items);
 
-cars = {"brand": "Ford", "model": "Mustang", "year": 1964, "color": "red", "price": 50000, "mileage": 10000, "engine": "V8", "transmission": "manual", "fuel": "gasoline", "doors": 2, "seats": 4, "weight": 1500, "acceleration": 5.0, "top_speed": 250, "horsepower": 300, "torque": 400, "fuel_efficiency": 10.0, "safety_rating": 5, "reliability_rating": 4, "popularity_rating": 4.5}
+#cars = {"brand": "Ford", "model": "Mustang", "year": 1964, "color": "red", "price": 50000, "mileage": 10000, "engine": "V8", "transmission": "manual", "fuel": "gasoline", "doors": 2, "seats": 4, "weight": 1500, "acceleration": 5.0, "top_speed": 250, "horsepower": 300, "torque": 400, "fuel_efficiency": 10.0, "safety_rating": 5, "reliability_rating": 4, "popularity_rating": 4.5}
 
-print(f"{cars}")
+#print(cars["brand"])
+
+user = { "items": ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"],
+         "name": "John Doe",
+         "age": 30,}
+print(user.get("age", 50))
+user2 = dict(name="Sarah Arsala", age=25)
+print(user2["name"], user2["age"])
+print(user2)
+
